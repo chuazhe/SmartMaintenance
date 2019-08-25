4f6ef570004b6990736597461b42d701b26b8184
Chua Wei Zhe <chuazhe@gmail.com>
2019-08-25 12:37:43 +0800

    Initial commit

---
+++ SmartMaintenance/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartMaintenance.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
