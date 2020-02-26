72996d5c8c09ac058f2f33e1a2d7f4016548d4c9
Chua Wei Zhe <chuazhe@gmail.com>
2020-02-26 23:00:50 +0800

    Update

--- SmartMaintenance/wwwroot/js/confirm.js
+++ SmartMaintenance/wwwroot/js/confirm.js
@@ -6,9 +6,17 @@
 
 
 function approveOrder() {
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = dd + '-' + mm + '-' + yyyy;
+
     $.ajax({
         type: "PUT",
-        url: uri + "api/order/approve/" + $('#routeDataId').val(),
+        url: uri + "api/order/approve/" + $('#routeDataId').val()+"/"+today,
         cache: false,
         async: false,
         error: function (jqXHR, textStatus, errorThrown) {
